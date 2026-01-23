d0376b6303aa82df4a66a4498a107de10cca67bc
zvwl <daniel.holecek20@gmail.com>
2026-01-23 20:30:54 +0000

    Add Google sign-in and Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import { AuthProvider } from './contexts/AuthContext'
 import './index.css'
 import App from './App.jsx'
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <Analytics />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
